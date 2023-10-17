0f79722061af4c7392f0c5c94ca243ed4755b995
Zuev91 <zionix007@gmail.com>
2023-10-17 22:25:44 +0300

    Initial commit

---
+++ Lesson06/Lesson06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson06
+//
+//  Created by Alexandr Zuev on 17.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
